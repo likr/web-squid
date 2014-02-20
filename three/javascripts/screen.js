94a6282b156275edc6d7caf6b947a5398392345d
Yoshihiro Ashida <y4ashida@gmail.com>
2014-02-20 16:54:11 +0900

    テストデータを読み込んで色を付けて表示する

--- three/javascripts/screen.js
+++ three/javascripts/screen.js
@@ -9,8 +9,8 @@ renderer.setClearColor(0xffffff, 1.0);
 stage.append(renderer.domElement);
 
 // initialize camera
-var zoom = 80,
-    camerax = 228,
+var zoom = 100,
+    camerax = 227.9,
     cameray = 158;
 var camera = new THREE.OrthographicCamera(rendererWidth/-zoom, rendererWidth/zoom, rendererHeight/zoom, rendererHeight/-zoom, 1, 1000);
 camera.position.set(camerax, cameray, 1);
@@ -21,7 +21,7 @@ var scene = new THREE.Scene();
 
 // mercator projection
 var r = 128 / Math.PI;
-var latitudeToPlane = function(lat){
+var latitudeToPlane = function (lat) {
   var latRad = Math.PI / 180 * lat;
   return r / 2 * Math.log((1.0+Math.sin(latRad))/(1.0-Math.sin(latRad))) + 128;
 };
@@ -31,7 +31,7 @@ var longitudeToPlane = function(lon){
 };
 
 var drawCoastLine = function(gml) {
-  var material = new THREE.LineBasicMaterial({ color: 0x16a085 });
+  var material = new THREE.LineBasicMaterial({ color: 0x000000 });
   $(gml).find('coastl').each(function() {
     var posList = $(this).find('posList')[0].innerHTML.split(' ');
     var geometry = new THREE.Geometry();
@@ -58,28 +58,22 @@ mesh.position = new THREE.Vector3(longitudeToPlane(135), latitudeToPlane(34.65),
 scene.add(mesh);
 
 // 三角形を2つ組み合わせて正方形を作る
-var drawSquare = function(coordinates, size, color16List) {
-  var triGeo = new THREE.Geometry();
-  var vertexCoordinatesList = [
-    [-1.0, 1.0],
-    [ 1.0, 1.0],
-    [ 1.0,-1.0],
-    [-1.0,-1.0],
-  ];
-  for(var i = 0; i < 2; i++) {
+var drawSquare = function(vertexCoordinatesList, color16List) {
+  var squGeo = new THREE.Geometry();
+  for (var i = 0; i < 2; i++) {
     for (var j = 0; j < 3; j++) {
       if(j==1&&i==1) {
         var k = 3;
       } else {
         var k = j;
       }
-      var x = size * vertexCoordinatesList[k][0];
-      var y = size * vertexCoordinatesList[k][1];
-      triGeo.vertices.push(new THREE.Vector3(x, y, 0));
+      var x = longitudeToPlane(vertexCoordinatesList[k][0]);
+      var y = latitudeToPlane(vertexCoordinatesList[k][1]);
+      squGeo.vertices.push(new THREE.Vector3(x, y, -1));
     }
   }
-  triGeo.faces.push(new THREE.Face3(0, 1, 2));
-  triGeo.faces.push(new THREE.Face3(3, 4, 5));
+  squGeo.faces.push(new THREE.Face3(0, 1, 2));
+  squGeo.faces.push(new THREE.Face3(3, 4, 5));
 
   for(var i = 0; i < 2; i++) {
     for (var j = 0; j < 3; j++) {
@@ -88,33 +82,119 @@ var drawSquare = function(coordinates, size, color16List) {
       } else {
         var k = j;
       }
-      triGeo.faces[i].vertexColors[j] = new THREE.Color(color16List[k]);
+      squGeo.faces[i].vertexColors[j] = new THREE.Color(color16List[k]);
     }
   }
-  var triMaterial = new THREE.MeshBasicMaterial({
+  var squMaterial = new THREE.MeshBasicMaterial({
       vertexColors:THREE.VertexColors,
       side:THREE.DoubleSide
   });
-  var triMesh = new THREE.Mesh(triGeo, triMaterial);
-  triMesh.position.set(longitudeToPlane(coordinates[1]), latitudeToPlane(coordinates[0]), -1);
-  console.log(triMesh);
-  scene.add(triMesh);
+  var squMesh = new THREE.Mesh(squGeo, squMaterial);
+  scene.add(squMesh);
 };
-drawSquare([38, 145], 2.0, [0xe74c3c, 0xecf0f1, 0x3498db, 0xf1c40f]);
 
-// cubeを上から見る
-var cubeGeo = new THREE.CubeGeometry(2, 2, 2);
-cubeGeo.faces[8].vertexColors[0] = new THREE.Color(0xecf0f1);
-cubeGeo.faces[8].vertexColors[1] = new THREE.Color(0xe74c3c);
-cubeGeo.faces[8].vertexColors[2] = new THREE.Color(0x3498db);
-cubeGeo.faces[9].vertexColors[0] = new THREE.Color(0xe74c3c);
-cubeGeo.faces[9].vertexColors[1] = new THREE.Color(0xf1c40f);
-cubeGeo.faces[9].vertexColors[2] = new THREE.Color(0x3498db);
-var cubeMaterial = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors });
-var cube = new THREE.Mesh(cubeGeo, cubeMaterial);
-cube.overdraw = true;
-cube.position.set(longitudeToPlane(145), latitudeToPlane(34), -10);
-scene.add(cube);
+// グリッド描写
+var drawGrid = function(xList, yList) {
+  var material = new THREE.LineBasicMaterial({ color: 0xaaaaaa }),
+      xmin = xList[0],
+      xmax = xList[xList.length-1],
+      ymin = yList[0],
+      ymax = yList[yList.length-1];
+
+  for (var i = xList.length - 1; i >= 0; i--) {
+    var geometry = new THREE.Geometry();
+    var x = xList[i];
+    var vertice = new THREE.Vector3(x, ymin, -0.5);
+    geometry.vertices.push(vertice);
+    var vertice = new THREE.Vector3(x, ymax, -0.5);
+    geometry.vertices.push(vertice);
+    var line = new THREE.Line(geometry, material);
+    scene.add(line);
+  }
+
+  for (var i = yList.length - 1; i >= 0; i--) {
+    var geometry = new THREE.Geometry();
+    var y = yList[i];
+    var vertice = new THREE.Vector3(xmin, y, -0.5);
+    geometry.vertices.push(vertice);
+    var vertice = new THREE.Vector3(xmax, y, -0.5);
+    geometry.vertices.push(vertice);
+    var line = new THREE.Line(geometry, material);
+    scene.add(line);
+  }
+};
+
+var xList = [], yList = [];
+$.ajax({
+  url: 'data/x.csv',
+  type: 'get',
+  dataType: 'html',
+  async: false,
+  success: function(lonListCsv) {
+    var lonList = CSV.parse(lonListCsv)[0];
+    for (var i = lonList.length - 1; i >= 0; i--) {
+      xList[i] = longitudeToPlane(lonList[i]);
+    }
+  }
+});
+$.ajax({
+  url: 'data/y.csv',
+  type: 'get',
+  dataType: 'html',
+  async: false,
+  success: function(latListCsv) {
+    var latList = CSV.parse(latListCsv)[0];
+    for (var i = latList.length - 1; i >= 0; i--) {
+      yList[i] = latitudeToPlane(latList[i]);
+    }
+  }
+});
+drawGrid(xList, yList);
+
+
+var rows;
+$.ajax({
+  url: 'data/kiri.csv',
+  type: 'get',
+  dataType: 'html',
+  async: false,
+  success: function(data) {
+    rows = CSV.parse(data);
+  }
+});
+
+function toColor(num) {
+  return + ("0x"+rainbow.colourAt(num));
+}
+
+var rainbow = new Rainbow();
+var len = rows.length,
+    max = 0,
+    min = 99;
+for (var i = len - 1; i >= 0; i--) {
+  if (max < rows[i][2]) { max = rows[i][2]; }
+}
+for (var i = len - 1; i >= 0; i--) {
+  if (min > rows[i][2]) { min = rows[i][2]; }
+}
+min = 34; // 決め打ち
+rainbow.setNumberRange(min, max);
+for (var i = 0, len = len - 443; i < len; i += 1) {
+  if (i != 441 && i % 442 != 441) {
+    var ulv = rows[i],
+        urv = rows[i+442],
+        lrv = rows[i+443],
+        llv = rows[i+1],
+        vertexCoordinatesList = [
+          [ulv[0], ulv[1]],
+          [urv[0], urv[1]],
+          [lrv[0], lrv[1]],
+          [llv[0], llv[1]]
+        ],
+        colors = [toColor(ulv[2]), toColor(urv[2]), toColor(lrv[2]), toColor(llv[2])];
+    drawSquare(vertexCoordinatesList, colors);
+  }
+}
 
 // render
 var render = function() {
